6f8e2ef9b02f56fe43dc640b0c0513bdb62da3b5
zhenyu <khoozhenyu@outlook.com>
2022-07-27 02:23:35 +0900

    new site

--- README.md
+++
@@ -1,8 +0,0 @@
-#My Personal Site!
-
-hello,
-
-this site will always be a work in progress. this site is used more to track my personal growth, of how much i've learnt, so enjoy!
-
-xoxo,
-Zhenyu
---
+++ js/main.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    document.getElementById("cond").style.color = "red";
+    document.getElementById("cond").style.fontSize = "30px";
+  }
+
+function changeContent(){
+    document.getElementById("cond").innerHTML = "THIS IS YOUR MOTHER!";
+}
