b152f5e8c76f992144173fcc3c406480fc404c86
zhenyu <khoozhenyu@outlook.com>
2023-05-29 22:38:40 +0900

    test

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function changeColor() {
-    document.getElementById("cond").style.color = "red";
-    document.getElementById("cond").style.fontSize = "30px";
-  }
-
-function changeContent(){
-    document.getElementById("cond").innerHTML = "THIS IS YOUR MOTHER!";
-}
